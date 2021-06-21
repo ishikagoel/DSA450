b697510bcfc0327d185b5c2171e8bc28580c49f8
ishika <ishikagoel8218@gmail.com>
2021-06-22 00:46:20 +0530

    Binary Search

---
+++ MedianOfTwoSortedArrayOfDifferentSizes.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+public class MedianOfTwoSortedArrayOfDifferentSizes {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int m = obj.nextInt();
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = obj.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            arr2[i] = obj.nextInt();
+        }
+        double ans=findMedianSortedArrays(arr1,arr2);
+        System.out.println(ans);;
+    }
+
+    public static double findMedianSortedArrays(int[] arr1, int[] arr2) 
+    {
+
+        if(arr2.length<arr1.length)
+        {
+            return findMedianSortedArrays(arr2,arr1);
+        }
+        int low=0;
+        int n1=arr1.length;
+        int n2=arr2.length;
+        int high=n1;
+        int div=(n1+n2+1)/2;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int cut1=mid;
+            int cut2=div-mid;
+            int l1= cut1==0?Integer.MIN_VALUE:arr1[cut1-1];
+            int l2= cut2==0?Integer.MIN_VALUE:arr2[cut2-1];
+
+            int r1= cut1==n1?Integer.MAX_VALUE:arr1[cut1];
+            int r2= cut2==n2?Integer.MAX_VALUE:arr2[cut2];
+
+
+            if(l1<=r2&&l2<=r1)
+            {
+                if((n1+n2)%2==0)
+                {
+                    return ((Math.max(l1,l2)+Math.min(r1,r2))/2.0);
+                }
+                else
+                {
+                    return Math.max(l1,l2);
+                }
+            }
+            else if(l1>r2)
+            {
+                high=cut1-1;
+                
+            }
+            else
+            {
+                low=cut1+1;
+            }
+            
+        }
+        return 0.0;
+    }
+}
--- searchElementInRotatedArray.java
+++ searchElementInRotatedArray.java
@@ -1,5 +1,48 @@
+import java.util.*;
+import java.io.*;
 public class searchElementInRotatedArray 
 {
-    
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int n=obj.nextInt();
+        int[] arr=new int[n];
+        int target=obj.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=obj.nextInt();
+        }
+        System.out.println(search(arr, target));
+    }
+    public static int search(int[] arr, int target) {
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(arr[mid]==target)
+            return mid;
+            if(arr[low]<arr[mid])
+            {
+                if(arr[low]<=target&&target<=arr[mid])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else
+            {
+                if(arr[mid]<=target&&target<=arr[high])
+                {
+                    low=mid+1;
+                }
+                else
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
     
 }
